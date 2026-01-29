fa295bff4d605165fde36b984b7da2ba06322a65
13 PoPaul <paul.iglesias--tallon@efrei.net>
2026-01-29 15:10:30 +0100

    Last commit for beta before Orcale new server

--- tic-tac.php
+++ tic-tac.php
@@ -4,8 +4,9 @@
    if ($age < 0)
    {
 	    $TimeLeft = "Error please try again";
-   } 
-   $TimeLeft = 80 - $age;
+   }
+   else $TimeLeft = 80 - $age;
+   
 ?>
 <html>
   <head>
@@ -13,11 +14,22 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="generator" content="PyScripter">
 
-	<link rel="stylesheet" type="text/css" href="02_style" />
+	<link rel="stylesheet" type="text/css" href="tic-tac.css" />
   </head>
   <body>
 	<p>
-		We estimate your time left to live is : <?php echo $TimeLeft; ?> years
+		We estimate your time left to live is : <?php echo $TimeLeft; ?> years<br>
+    Every black block is a year of your life !
+  <?php
+    for ($i = 0; $i < $age; $i++)
+    {
+        echo '<span class="BBlock"></span>';
+    }
+    for ($j = $age; $j < 80; $j++)
+    {
+        echo '<span class="WBlock"></span>';
+    }
+  ?>
 		<form action="index.php">
 			<input class="" type="submit" value="Home">
 		</form>
