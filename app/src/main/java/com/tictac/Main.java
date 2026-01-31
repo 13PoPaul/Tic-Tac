ab05964c8c263c1ffb641051f8774f52216982c3
13 PoPaul <paul.iglesias--tallon@efrei.net>
2026-01-31 16:24:54 +0100

    New Structure for koyeb try

---
+++ app/src/main/java/com/tictac/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // C'est ici que ton jeu commence vraiment
+        System.out.println("Serveur Oracle prêt à recevoir la logique du jeu !");
+        
+        // Appelle ici tes fonctions de jeu
+    }
+}
