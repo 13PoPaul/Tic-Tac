d1b23103c87d90d4aa1f87b556145d2d9bfc349d
PoPaul <paul.iglesias--tallon@efrei.net>
2026-01-31 16:27:30 +0100

    Delete backend directory

--- backend/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("--- Tic-Tac Backend : INITIALISÉ ---");
-        System.out.println("Le moteur de calcul d'espérance de vie est prêt.");
-    }
-}
