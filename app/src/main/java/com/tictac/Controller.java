1f7484b1cd4980cbdb310376b663224d15e4ff5a
13 PoPaul <paul.iglesias--tallon@efrei.net>
2026-02-02 23:04:49 +0100

    Nouvelle page web résultat + fichier java pour gérer les fichiers html + tentative envoie d'information d'une page a l'autre

---
+++ app/src/main/java/com/tictac/Controller.java
@@ -0,0 +1,26 @@
+package com.tictac; // 1. L'adresse
+
+import org.springframework.stereotype.Controller; // 2. L'outil
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+
+@Controller // 3. L'étiquette magique
+public class QuestionController 
+{
+
+    @GetMapping("/") // 1. On écoute l'adresse principale
+    public String accueil() 
+    {
+        return "index";
+    }
+    @PostMapping("/result") // 2. On récupère les données du formulaire
+    public String result(@RequestParam("Sex") int sex Model model) 
+    {
+        model.addAttribute("SexChosen", sex);
+        return "result";
+    }
+
+}
